b8dc72880f0fbbfa880f2e8131d11b744c87ffd9
outfrost <kotlet.bahn@gmail.com>
2020-10-05 00:41:13 +0200

    Animate text display in narrative popup

--- game/NarrativePopup.gd
+++ game/NarrativePopup.gd
@@ -1,8 +1,23 @@
 class_name NarrativePopup
 extends Popup
 
-func display(msg: String, duration: float) -> void:
-	$Panel/RichTextLabel.bbcode_text = msg
+const TIME_PER_CHAR: float = 0.04
+
+var time_displayed: float = 0.0
+var duration: float = 0.0
+var message: String
+
+func _process(delta: float) -> void:
+	if !visible:
+		return
+	time_displayed += delta
+	if time_displayed > duration:
+		hide()
+	$Panel/RichTextLabel.bbcode_text = message.substr(0, time_displayed / TIME_PER_CHAR)
+
+func display(msg: String, dur: float) -> void:
+	message = msg
+	duration = dur
+	time_displayed = 0.0
+	$Panel/RichTextLabel.bbcode_text = ""
 	show()
-	yield(get_tree().create_timer(duration), "timeout")
-	hide()
