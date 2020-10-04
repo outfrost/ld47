6890fdacf473fd4f80933ff9bdaca966959ffbc8
savenger <github@smahmood.de>
2020-10-04 02:50:39 +0200

    projectiles/holes

---
+++ levels/Projectile.gd
@@ -0,0 +1,29 @@
+extends KinematicBody
+
+const MAX_LIFE_TIME: int = 1
+const SPEED: int = 2
+var direction: Vector3
+var life_time: float = 0
+var collided: bool = false
+
+func _ready():
+	pass
+
+func _process(delta):
+	if not collided:
+		var collision = move_and_collide(direction * SPEED, false)
+		if collision:
+			# TODO:
+			# if projectile hits the player, it stays in place were it hit the player
+			# good would be, if it follows the player (get stuck inside the player)
+			# the following code needs the correct calculation of the position/origin
+			#get_parent().remove_child(self)
+			#collision.collider.add_child(self)
+			#global_transform.origin = collision.collider.global_transform.origin - global_transform.origin
+			life_time = 0
+			collided = true
+			$Particles.emitting = false
+	else:
+		life_time += delta
+		if life_time > MAX_LIFE_TIME:
+			queue_free()
