c487fe4c2d2bee1adf26a885007291caeed40fb6
outfrost <kotlet.bahn@gmail.com>
2020-10-05 10:50:26 +0200

    Add credits

--- game/menu/CreditsPopup.gd
+++ game/menu/CreditsPopup.gd
@@ -1,5 +1,9 @@
 extends Popup
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		hide()
+
 func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and !event.pressed:
