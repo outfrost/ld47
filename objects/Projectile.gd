e2a7cf7da261c0931dfa6ddd9c65e58af1f6c929
outfrost <kotlet.bahn@gmail.com>
2020-10-05 06:14:07 +0200

    Add travel distance limiting to projectiles

--- objects/Projectile.gd
+++ objects/Projectile.gd
@@ -1,33 +1,34 @@
 extends KinematicBody
 
-const MAX_LIFE_TIME: float = 1.0
+const MAX_TRAVEL_DIST: float = 14.0
 const SPEED: float = 20.0
 var direction: Vector3
-var life_time: float = 0
 var collided: bool = false
+var distance_travelled: float = 0.0
 
 func _ready():
 	pass
 
 func _process(delta):
 	if not collided:
-		var collision = move_and_collide(delta * direction * SPEED, false)
+		var distance = SPEED * delta
+		distance_travelled += distance
+		if distance_travelled > MAX_TRAVEL_DIST:
+			queue_free()
+			distance -= distance_travelled - MAX_TRAVEL_DIST
+		var collision = move_and_collide(direction * distance, false)
 		if collision:
-			# TODO:
+			# TODO maybe
 			# if projectile hits the player, it stays in place were it hit the player
 			# good would be, if it follows the player (get stuck inside the player)
 			# the following code needs the correct calculation of the position/origin
+			collided = true
+			queue_free()
 			if collision.collider.has_method("die"):
 				collision.collider.die("projectile")
 			#get_parent().remove_child(self)
 			#collision.collider.add_child(self)
 			#global_transform.origin = collision.collider.global_transform.origin - global_transform.origin
-			life_time = 0
-			collided = true
-			$Particles.emitting = false
-			collision_layer = 0
-			collision_mask = 0
-	else:
-		life_time += delta
-		if life_time > MAX_LIFE_TIME:
-			queue_free()
+#			$Particles.emitting = false
+#			collision_layer = 0
+#			collision_mask = 0
