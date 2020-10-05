b34133bfc819e2a87162f5ed0713641175355c5f
outfrost <kotlet.bahn@gmail.com>
2020-10-06 00:20:12 +0200

    Fix fire sizzle sound trigger

--- objects/Fire.gd
+++ objects/Fire.gd
@@ -10,12 +10,12 @@ func _process(delta):
 	$Particles.emitting = emitting
 	$Light.visible = emitting
 	if character and emitting:
+		$AudioStreamPlayer3D.play()
 		character.die("fire")
 
 func _on_Area_body_entered(body):
 	if body.has_method("die"):
 		character = body
-		$AudioStreamPlayer3D.play()
 
 func _on_Area_body_exited(body: Node) -> void:
 	if body == character:
