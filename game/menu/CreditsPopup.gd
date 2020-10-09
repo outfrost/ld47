744c84891de892f1cab0cf55454cc588b3bf9d5a
outfrost <kotlet.bahn@gmail.com>
2020-10-09 13:05:32 +0200

    Link LD47 submission in credits
    Closes #11

--- game/menu/CreditsPopup.gd
+++ game/menu/CreditsPopup.gd
@@ -1,5 +1,8 @@
 extends Popup
 
+func _ready() -> void:
+	$Panel/RichTextLabel.connect("meta_clicked", self, "on_meta_clicked")
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		hide()
@@ -8,3 +11,7 @@ func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and !event.pressed:
 			hide()
+
+func on_meta_clicked(meta: String):
+	if meta.begins_with("http"):
+		OS.shell_open(meta)
