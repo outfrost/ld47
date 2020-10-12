8bc24e17fd877e4efcf41a1235ea1a60d8f878a1
outfrost <kotlet.bahn@gmail.com>
2020-10-12 03:31:43 +0200

    Use built-in property for text animation

--- game/NarrativePopup.gd
+++ game/NarrativePopup.gd
@@ -3,9 +3,10 @@ extends Popup
 
 const TIME_PER_CHAR: float = 0.03
 
+onready var label = $Panel/RichTextLabel
+
 var time_displayed: float = 0.0
 var duration: float = 0.0
-var message: String
 
 func _process(delta: float) -> void:
 	if !visible:
@@ -13,11 +14,11 @@ func _process(delta: float) -> void:
 	time_displayed += delta
 	if time_displayed > duration:
 		hide()
-	$Panel/RichTextLabel.bbcode_text = message.substr(0, time_displayed / TIME_PER_CHAR)
+	label.visible_characters = time_displayed / TIME_PER_CHAR
 
 func display(msg: String, dur: float) -> void:
-	message = msg
 	duration = dur
 	time_displayed = 0.0
-	$Panel/RichTextLabel.bbcode_text = ""
+	label.bbcode_text = msg
+	label.visible_characters = 0
 	show()
